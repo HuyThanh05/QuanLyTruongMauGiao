b1fb10cd4ed3beafdda227b2666e3f0a4311e3d9
Khoi-Le-dev <87595938+rifujin123@users.noreply.github.com>
2025-11-11 00:02:15 +0700

    complete login register

--- app/controllers/routeController.py
+++ app/controllers/routeController.py
@@ -1,34 +1,31 @@
-from flask import Blueprint, render_template, request, jsonify
-from werkzeug.security import generate_password_hash, check_password_hash
-from app.models.users import User
-from app import db
+from flask import Blueprint, render_template
 
-routes = Blueprint('routes', __name__)
+routeController = Blueprint('routeController', __name__)
 
-@routes.route('/')
+@routeController.route('/')
 def home():
     return render_template('home.html',Title='Trang chủ')
 
-@routes.route('/student')
+@routeController.route('/student')
 def student():
     return render_template('student.html',Title='Danh sách học sinh')
 
-@routes.route('/health')
+@routeController.route('/health')
 def health():
     return render_template('health.html',Title='Sức khỏe')
 
-@routes.route('/schedule')
+@routeController.route('/schedule')
 def schedule():
     return render_template('schedule.html',Title='Lịch học')
 
-@routes.route('/fee')
+@routeController.route('/fee')
 def fee():
     return render_template('fee.html',Title='Học phí')
 
-@routes.route('/report')
+@routeController.route('/report')
 def report():
     return render_template('report.html',Title='Báo cáo')
 
-@routes.route('/signup', methods=['GET', 'POST'])
+@routeController.route('/signup', methods=['GET'])
 def signup():
     return render_template('login.html')
