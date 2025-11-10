e7796a5b8548c6ab91b373f380d42a9f63f907b7
Khoi-Le-dev <87595938+rifujin123@users.noreply.github.com>
2025-11-10 22:59:59 +0700

    refactor

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -5,64 +5,30 @@ from app import db
 
 routes = Blueprint('routes', __name__)
 
-# Route mặc định: chuyển về dashboard
 @routes.route('/')
-def index():
-    return render_template('home.html')
+def home():
+    return render_template('home.html',Title='Trang chủ')
 
-# Route chức năng
+@routes.route('/student')
+def student():
+    return render_template('student.html',Title='Danh sách học sinh')
 
-@routes.route('/dashboard')
-def dashboard():
-    return render_template('home.html')
+@routes.route('/health')
+def health():
+    return render_template('health.html',Title='Sức khỏe')
+
+@routes.route('/schedule')
+def schedule():
+    return render_template('schedule.html',Title='Lịch học')
+
+@routes.route('/fee')
+def fee():
+    return render_template('fee.html',Title='Học phí')
+
+@routes.route('/report')
+def report():
+    return render_template('report.html',Title='Báo cáo')
 
 @routes.route('/signup', methods=['GET', 'POST'])
 def signup():
     return render_template('login.html')
-
-#Endpoint login/register
-
-def validate_register_payload(data):
-    phone = data.get('phone')
-    email = data.get('email')
-    password = data.get('password')
-
-    errors = []
-    if not phone:
-        errors.append('Phone is required')
-    if not email:
-        errors.append('Email is required')
-    if not password:
-        errors.append("Password is required")
-    return errors, phone, email, password
-
-@routes.route('/api/auth/register', methods=['POST'])
-def api_register():
-    data = request.get_json(silent=True) or {}
-    errors, phone, email, password = validate_register_payload(data)
-    if errors:
-        return jsonify(
-            success=False,
-            message="Validation Failed",
-            errors=errors
-        ), 400
-
-    # Check if phone or email already exists in the database
-    existing_user = User.query.filter((User.phone == phone) | (User.email == email)).first()
-    if existing_user:
-        return jsonify(
-            success=False,
-            message='Phone number or email already exists'
-        ), 400
-
-    user = User(phone=phone, email=email, password_hash=generate_password_hash(password))
-    db.session.add(user)
-    db.session.commit()
-    return jsonify(
-        success=True,
-        message='Registration successful'
-    ), 201
-
-# @routes.route('/api/auth/login', methods=['POST'])
-# def api_login():
-    
