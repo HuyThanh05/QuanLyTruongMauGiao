b8b5efb0b6f9dba1958753fc58da40d717fef950
HuyThanh05 <nguyennhuythanh05@gmail.com>
2026-01-20 11:39:03 +0700

    fix

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from app import create_app
+from app import create_app, db
+from flask_migrate import upgrade
 
 app = create_app()
 
 if __name__ == "__main__":
+    with app.app_context():
+        upgrade()
+
     app.run(debug=True)
